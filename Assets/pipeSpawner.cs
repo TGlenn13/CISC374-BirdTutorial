4d0685ad8188287197b818917eb5af3b6c75f747
Thomas Glenn <tglenn@udel.edu>
2025-02-28 17:49:07 -0500

    Added random color to pipes

--- Assets/pipeSpawner.cs
+++ Assets/pipeSpawner.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class pipeSpawner : MonoBehaviour
 {
     public GameObject pipe;
-    public float spwanRate = 2;
+    public float spwanRate = 2f;
     private float timer = 0;
-    public float heightOffset = 10;
+    public float heightOffset = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,6 +25,23 @@ public class pipeSpawner : MonoBehaviour
     void spawnPipe() {
         float lowestPoint = transform.position.y - heightOffset;
         float highestPoint = transform.position.y + heightOffset;
-        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0),transform.rotation);
+        GameObject newPipe = Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+        ApplyRandomColor(newPipe);
+    }
+    void ApplyRandomColor(GameObject pipeObject) {
+{
+        // ChatGPT
+        // Get all SpriteRenderers in the pipe (including children)
+        SpriteRenderer[] spriteRenderers = pipeObject.GetComponentsInChildren<SpriteRenderer>();
+
+        // Generate a random bright color
+        Color randomColor = new Color(Random.Range(0.5f, 1f), Random.Range(0.5f, 1f), Random.Range(0.5f, 1f));
+        //ChatGPT end
+
+        // Apply the same random color to all sprite renderers (top & bottom pipe)
+        foreach (SpriteRenderer sr in spriteRenderers){
+            sr.color = randomColor;
+            }
+        }
     }
 }
