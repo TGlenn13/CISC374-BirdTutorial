d27e41122a6dd9218192a0d38c3151dd8042d44b
Thomas Glenn <tglenn@udel.edu>
2025-02-28 18:39:36 -0500

    Added contrail

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,11 +6,12 @@ public class BirdScript : MonoBehaviour
     public float flapStrength;
     public LogicScript logic;
     public bool birdIsAlive = true;
+    public ParticleSystem birdTrail;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
-        
+        birdTrail = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -18,12 +19,14 @@ public class BirdScript : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive) {
         myRigidBody.linearVelocity = Vector2.up * flapStrength;
+        birdTrail.Play();
         }
         Vector3 screenPosition = Camera.main.WorldToViewportPoint(transform.position);
         if (screenPosition.y > 1 || screenPosition.y < 0) 
         {
             birdIsAlive = false;
             logic.gameOver();
+            birdTrail.Stop();
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
